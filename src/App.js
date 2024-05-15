9503a1929fccad286b12c3286252d23ff2c40021
anwinwindfall <anwina@windfallproductions.org>
2024-05-15 17:43:00 +0530

    content-cms

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.scss';
 import Widget from './components/Widget/Widget';
+import Content from './components/Content/Content';
 
 function App({ moduleData }) {
   
   
   return (
-    <div className="swf-donation">
-      {console.log(moduleData)}
+    <div className="swf-donation-container">
+      <Content content={moduleData.content}/>
       <Widget productId={moduleData.product_id} planId={moduleData.plan_id} buttonSettings={moduleData.button_settings} submitBtnColor={moduleData.donate_button_color.css} currencyCode={moduleData.currencyCode} cart={moduleData.cart_url} submitBtnText={moduleData.submit_button_text}/>
     </div>
   );
